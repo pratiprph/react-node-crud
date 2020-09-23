04a912126eb2e522d710b689848498350a01ec2d
pratrph <pratipsinha@gmail.com>
2020-09-23 00:26:18 -0700

    final commit

---
+++ db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('MONGO URL',{useNewUrlParser:true,useUnifiedTopology:true},
+    err => {
+        if (!err)
+            console.log('Mongodb connection succeeded.')
+        else
+            console.log('Error while connecting MongoDB : ' + JSON.stringify(err, undefined, 2))
+    })
